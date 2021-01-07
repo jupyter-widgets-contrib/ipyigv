43734da7679d6c96cd47b632f32b98dd27d7b50c
Mariana Meireles <mariana@psychonautgirl.space>
2021-01-07 13:09:47 +0000

    Fix typo

--- ipyigv/_version.py
+++ ipyigv/_version.py
@@ -2,7 +2,7 @@
 version_info = (0, 1, 0)
 
 # Module version accessible using ipyigv.__version__
-__version__ = '%s.%s.%s%s'%(version_info[0], version_info[1], version_info[2],
+__version__ = '%s.%s.%s%s'%(version_info[0], version_info[1], version_info[2])
 
 
 # Frontend version
