db5e3bb379b744a98c590077f0e974dc329e93ad
Mariana Meireles <mariana@psychonautgirl.space>
2021-01-11 11:43:52 +0000

    Version 0.1.6

--- ipyigv/_version.py
+++ ipyigv/_version.py
@@ -1,9 +1,9 @@
 # Module version
-version_info = (0, 1, 5)
+version_info = (0, 1, 6)
 
 # Module version accessible using ipyigv.__version__
 __version__ = '%s.%s.%s'%(version_info[0], version_info[1], version_info[2])
 
 
 # Frontend version
-EXTENSION_VERSION = '0.1.5'
+EXTENSION_VERSION = '0.1.6'
