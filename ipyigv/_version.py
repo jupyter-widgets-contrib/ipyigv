4e521941e34038ff9c9c860d2ef79f14f8d003d2
Mariana Meireles <mariana@psychonautgirl.space>
2021-01-19 13:27:51 +0000

    Version 0.1.8

--- ipyigv/_version.py
+++ ipyigv/_version.py
@@ -1,5 +1,5 @@
 # Module version
-version_info = (0, 1, 7)
+version_info = (0, 1, 8)
 
 # Module version accessible using ipyigv.__version__
 __version__ = '%s.%s.%s'%(version_info[0], version_info[1], version_info[2])
