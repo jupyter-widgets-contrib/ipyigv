1647351d83c980213ced2be235da7ce39eb30c5a
Sylvain Corlay <sylvain.corlay@gmail.com>
2021-02-16 23:33:51 +0100

    Minor fixes, removal of utils.py, formatting issues

--- ipyigv/ipyigv.py
+++ ipyigv/ipyigv.py
@@ -1,61 +1,49 @@
 import json
-import ipywidgets as widgets
 
+from ipywidgets import DOMWidget, Output, Widget, register, widget_serialization
 from ipywidgets.widgets.trait_types import InstanceDict
 
-from traitlets import (
-    Float, Unicode, Int, Tuple, List, Instance, Bool, Dict, Enum,
-    link, observe, default, validate, TraitError, Union, HasTraits
-)
-
+from traitlets import Unicode, Int, List, Instance, Bool, validate, TraitError
 from traitlets.utils.bunch import Bunch
 
 from .options import *
 
-from .utils import widget_serialization_no_none
 from ._version import EXTENSION_VERSION
 
 PUBLIC_GENOMES_FILE = os.path.join(os.path.dirname(__file__), 'public_genomes.json')
 PUBLIC_GENOMES = Bunch({v['id']: v for v in json.load(open(PUBLIC_GENOMES_FILE, 'r')) } )
 
 
-@widgets.register
-class IgvBrowser(widgets.DOMWidget):
+@register
+class IgvBrowser(DOMWidget):
     """An IGV browser widget."""
-
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.on_msg(self._custom_message_handler)
 
-    out = widgets.Output()
+    out = Output()
 
-    # Name of the widget view class in front-end
     _view_name = Unicode('IgvBrowser').tag(sync=True)
-    # Name of the widget model class in front-end
     _model_name = Unicode('IgvModel').tag(sync=True)
-    # Name of the front-end module containing widget view
     _view_module = Unicode('jupyter-igv').tag(sync=True)
-    # Name of the front-end module containing widget model
     _model_module = Unicode('jupyter-igv').tag(sync=True)
-    # Version of the front-end module containing widget view
     _view_module_version = Unicode(EXTENSION_VERSION).tag(sync=True)
-    # Version of the front-end module containing widget model
     _model_module_version = Unicode(EXTENSION_VERSION).tag(sync=True)
 
-    # Widget specific property.
+    # Widget-specific property.
     # Widget properties are defined as traitlets. Any property tagged with `sync=True`
     # is automatically synced to the frontend *any* time it changes in Python.
     # It is synced back to Python from the frontend *any* time the model is touched.
-    genome = InstanceDict(ReferenceGenome).tag(sync=True, **widget_serialization_no_none)
-    tracks = List(InstanceDict(Track)).tag(sync=True, **widget_serialization_no_none)
+    genome = InstanceDict(ReferenceGenome).tag(sync=True, **widget_serialization)
+    tracks = List(InstanceDict(Track)).tag(sync=True, **widget_serialization)
     doubleClickDelay = Int(default_value=500).tag(sync=True)
     flanking = Int(default_value=1000).tag(sync=True)
-    genomeList = Unicode(allow_none=True).tag(sync=True, **widget_serialization_no_none)  # optional URL
-    locus = (Unicode() | List(Unicode())).tag(sync=True, **widget_serialization_no_none)
+    genomeList = Unicode(allow_none=True).tag(sync=True, **widget_serialization)  # optional URL
+    locus = (Unicode() | List(Unicode())).tag(sync=True, **widget_serialization)
     minimumBases = Int(default_value=40).tag(sync=True)
     queryParametersSupported = Bool(default=False).tag(sync=True)
-    search = InstanceDict(SearchService, allow_none=True).tag(sync=True, **widget_serialization_no_none)
+    search = InstanceDict(SearchService, allow_none=True).tag(sync=True, **widget_serialization)
     showAllChromosomes = Bool(default_value=True).tag(sync=True)
     showAllChromosomeWidget = Bool(default_value=True).tag(sync=True)
     showNavigation = Bool(default_value=True).tag(sync=True)
@@ -63,7 +51,7 @@ class IgvBrowser(widgets.DOMWidget):
     showRuler = Bool(default_value=True).tag(sync=True)
     showCenterGuide = Bool(default_value=False).tag(sync=True)
     # trackDefaults = # missing documentation
-    roi = List(InstanceDict(AnnotationTrack)).tag(sync=True, **widget_serialization_no_none) # regions of interest
+    roi = List(InstanceDict(AnnotationTrack)).tag(sync=True, **widget_serialization) # regions of interest
     oauthToken = Unicode(allow_none = True).tag(sync=True)
     apiKey = Unicode(allow_none = True).tag(sync=True)
     clientId = Unicode(allow_none = True).tag(sync=True)
@@ -105,10 +93,3 @@ class IgvBrowser(widgets.DOMWidget):
     def _return_json_handler(self, content):
         print (content['json'])
 
-    # @validate('roi')
-    # def _valid_roi(self, roi):
-    #     if isinstance(roi, AnnotationTrack)):
-    #         return roi
-    #     else:
-    #         print("validating roi: type %s", type(roi))
-    #         raise TraitError("Regions of Interest must be of type AnnotationTrack")
